f34cda949fccfcbc1b8a3d1900d96bba9dbcb2fa
neutralmike <neutralmike@yandex.ru>
2020-04-09 18:35:43 +0300

    100% for ax^2+b

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,11 +3,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import explained_variance_score
 
 import pandas as pd
+import numpy as np
 
 raw_data = pd.read_csv('data.csv',)
-X = raw_data[['a', 'b', 'x']]
-y = raw_data['y']
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-lr = LinearRegression()
-lr.fit(X_train, y_train)
-print(explained_variance_score(y_test, lr.predict(X_test)))
+scores = []
+
+for i in range(-100, 101):
+    X = raw_data[raw_data['x'] == i][['a', 'b']]
+    y = raw_data[raw_data['x'] == i]['y']
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+    scores.append(explained_variance_score(y_test, lr.predict(X_test)))
+
+print(np.mean(scores))
--- make_data_csv.py
+++ make_data_csv.py
@@ -9,27 +9,14 @@ data_dict = {
 }
 
 for main_for_index in range(100):
-    dots = []
-    a = int((random.random() - 0.5) * 100 // 1)
-    b = int((random.random() - 0.5) * 100 // 1)
+    a = int((random.random() - 0.5) * 100)
+    b = int((random.random() - 0.5) * 100)
 
-    for a_for_index in range(100):
-        varying_a = int((random.random() - 0.5) * 100 // 1)
-        varying_b = int((random.random() - 0.5) * 100 // 1)
-
-        for x_y_for_index in range(-100, 100):
-            data_dict['a'].append(varying_a)
-            data_dict['b'].append(b)
-            data_dict['x'].append(x_y_for_index)
-            data_dict['y'].append(varying_a * x_y_for_index ** 2 + b)
-
-        for x_y_for_index in range(-100, 100):
-            data_dict['a'].append(a)
-            data_dict['b'].append(varying_b)
-            data_dict['x'].append(x_y_for_index)
-            data_dict['y'].append(a * x_y_for_index ** 2 + varying_b)
-
-
+    for x_for_index in range(-100, 101):
+        data_dict['a'].append(a)
+        data_dict['b'].append(b)
+        data_dict['x'].append(x_for_index)
+        data_dict['y'].append(a * x_for_index ** 2 + b)
 
 df = pd.DataFrame(data_dict)
 
