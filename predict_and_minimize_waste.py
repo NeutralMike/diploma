82c97293788e3c47ca120f82d690c9e897a6d005
neutralmike <neutralmike@yandex.ru>
2020-05-17 19:33:40 +0300

    new version for step 3 with more statistic

--- predict_and_minimize_waste.py
+++ predict_and_minimize_waste.py
@@ -29,73 +29,54 @@ water_y_train = train_data['Fв']
 water_model = GradientBoostingRegressor()
 water_model.fit(water_X_train, water_y_train)
 
-print('среднее значение было бы :', (predict_data['Fг']/predict_data['Fв']).mean())
+real_mean = (predict_data['Fг']/predict_data['Fв']).mean()
+features_with_max_min = {
+    'ИМП' : range(0, 101, 5),
+    'ИМГ' : range(0, 101, 5),
+    'Pвэ': np.arange(2, 6.1, 0.1),
+    'fдым' : range(20, 51),
+    'Pдэ' : range(-500, 1, 5),
+    'Рго' : np.arange(0, 8.1, 0.1),
+    'Tвэ' : range(0, 181),
+    'Tдк' : range(50, 501, 5),
+}
 
-minimals = []
-how_much_changed = []
-for _, row in predict_data.iterrows():
-    temp_row = row.copy()
+print('среднее значение было бы :', real_mean)
+
+for feature_name, feature_range in features_with_max_min.items():
     start_time = time.time()
-    minimal = {'row': row, 'gaz/water': row['Fг']/row['Fв']}
-    # print('Было бы:', minimal['gaz/water'])
-    temp_row['ИМГ'] = 100.0
-    temp_row['ИМП'] = 100.0
-    # temp_row['Pвэ'] = 5.2
-    # temp_row['fдым'] = 50
-    # temp_row['Tдк'] = 500.
-    # for pve in np.arange(2, 6.1, 0.1):
-    #     temp_row['Pвэ'] = pve
-    #     for img in range(0, 101, 5):
-    #         temp_row['ИМГ'] = img
-    # for imp in range(0, 101, 5):
-    #     temp_row['ИМП'] = imp
-    #     for f_smoke in range(20, 51):
-    #         temp_row['fдым'] = f_smoke
-    # for pde in range(-500, 1, 5):
-    #     temp_row['Pдэ'] = pde
-    #     for pgo in np.arange(0, 8.1, 0.1):
-    #         temp_row['Рго'] = pgo
-    # for tve in range(0, 181):
-    #     temp_row['Tвэ'] = tve
-    # for pve in np.arange(2, 6.1, 0.1):
-    #     temp_row['Pвэ'] = pve
-    #     for img in range(0, 101, 5):
-    #         temp_row['ИМГ'] = img
-    # for imp in range(0, 101, 5):
-    #     temp_row['ИМП'] = imp
-    #     for f_smoke in range(20, 51):
-    #         temp_row['fдым'] = f_smoke
-    # for pde in range(-500, 1, 5):
-    #     temp_row['Pдэ'] = pde
-    #     for pgo in np.arange(0, 8.1, 0.1):
-    #         temp_row['Рго'] = pgo
-    # for tve in range(0, 141, 5):
-    #     temp_row['Tвэ'] = tve
-    #     for tdk in range(50, 501, 5):
-    #         temp_row['Tдк'] = tdk
-    gaz_predict = gaz_model.predict(np.array(temp_row[['Pвэ', 'Рпб', 'ИМГ', 'fдым', 'Pдэ', 'Рго']]).reshape((1, -1)))[0]
-    water_predict = water_model.predict(np.array(temp_row[['Pвэ', 'Рпб', 'Tвэ', 'ИМП', 'Tдк']]).reshape((1, -1)))[0]
-    gaz_water = gaz_predict/water_predict
-    if gaz_water < minimal['gaz/water']:
-        minimal['gaz/water'] = gaz_water
-        minimal['row'] = temp_row
+    minimals = []
+    new_values = []
+    how_much_changed = 0
+    not_touched_values = []
+    for _, row in predict_data.iterrows():
+        temp_row = row.copy()
+        minimal = {'row': row, 'gaz/water': row['Fг'] / row['Fв'], 'new_value': 0}
+        for feature_value in feature_range:
+            temp_row[feature_name] = feature_value
+            gaz_predict = gaz_model.predict(np.array(temp_row[['Pвэ', 'Рпб', 'ИМГ', 'fдым', 'Pдэ', 'Рго']]).reshape((1, -1)))[0]
+            water_predict = water_model.predict(np.array(temp_row[['Pвэ', 'Рпб', 'Tвэ', 'ИМП', 'Tдк']]).reshape((1, -1)))[0]
+            gaz_water = gaz_predict/water_predict
+            if gaz_water < minimal['gaz/water']:
+                minimal['gaz/water'] = gaz_water
+                minimal['row'] = temp_row
+                minimal['new_value'] = temp_row[feature_name]
 
-    minimals.append(minimal['gaz/water'])
-    if minimal['gaz/water'] != row['Fг']/row['Fв']:
-        how_much_changed.append(1)
-    else:
-        how_much_changed.append(0)
+        minimals.append(minimal['gaz/water'])
+        if minimal['gaz/water'] != row['Fг'] / row['Fв']:
+            how_much_changed += 1
+            new_values.append(minimal['row'][feature_name])
+        else:
+            not_touched_values.append(row[feature_name])
 
-    # print('При:\n',
-    #       'Pвэ  ', row['Pвэ'], ' --> ', minimal['row']['Pвэ'], '\n',
-    #       'ИМГ  ', row['ИМГ'], ' --> ', minimal['row']['ИМГ'], '\n',
-    #       'ИМП  ', row['ИМП'], ' --> ', minimal['row']['ИМП'], '\n',
-    #       'fдым  ', row['fдым'], ' --> ', minimal['row']['fдым'], '\n',
-    #       'Pдэ  ', row['Pдэ'], ' --> ', minimal['row']['Pдэ'], '\n',
-    #       'Рго  ', row['Рго'], ' --> ', minimal['row']['Рго'], '\n',
-    #       'Tвэ  ', row['Tвэ'], ' --> ', minimal['row']['Tвэ'], '\n',
-    #       'Tдк  ', row['Tдк'], ' --> ', minimal['row']['Tдк'], '\n',
-    #       '\n Будет:', minimal['gaz/water'],
-    #       '\n', time.time() - start_time)
-print('среднее значение: ', np.array(minimals).mean())
-print('изменено ', sum(how_much_changed), '/', len(how_much_changed))
+    print('{0} уменьшило отношение на {1:.2f}% ( {2} --> {3}).\n'
+          '    Изменило {4}/1000.\n'
+          '    Среднее новое значение для измненных строк {5}\n'
+          '    Среднее значение неизмененных строк {6}\n'
+          '    Время вычисления: {7}\n'.format(
+            feature_name, 100 * ((real_mean - np.mean(minimals)) / real_mean), real_mean, np.mean(minimals),
+            how_much_changed,
+            np.mean(new_values),
+            np.mean(not_touched_values),
+            time.time() - start_time,
+            ))
