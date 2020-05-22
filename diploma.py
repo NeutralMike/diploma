841123c287ba9a2418f647e2117024a369bfaccd
neutralmike <neutralmike@yandex.ru>
2020-05-22 23:56:52 +0300

    comment in diploma x_col_list and use drop

--- diploma.py
+++ diploma.py
@@ -29,8 +29,10 @@ features_of_y = {
 for y_col, x_cols_list in features_of_y.items():
     y_train = raw_data[y_col]
     X_train = raw_data.drop(columns=[y_col, 'Примечание'])
+    # X_train = raw_data[x_cols_list]
     y_test = test_data[y_col]
     X_test = test_data.drop(columns=[y_col, 'Примечание'])
+    # X_test = test_data[x_cols_list]
     regr = LinearRegression()
     regr.fit(X_train, y_train)
     elastic = ElasticNet()
